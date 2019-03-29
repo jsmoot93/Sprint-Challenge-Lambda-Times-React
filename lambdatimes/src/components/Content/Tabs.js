95d1adddf11979070f566da11d09e15b52c2bacc
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-03-29 13:17:22 -0400

    need to do styled components for mvp

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +8,23 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            {props.tabs.map(tab => (
+              <Tab
+              selectTabHandler={props.selectTabHandler}
+              selectedTab={props.selectedTab}
+              tab={tab}
+              filterCards={props.filterCards}
+              />
+            ))}
       </div>
     </div>
   );
 };
-
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+	selectTabHandler: PropTypes.func,
+	selectedTab: PropTypes.string,
+  tab: PropTypes.arrayOf(PropTypes.string),
+  filterCards: PropTypes.func
+};
 export default Tabs;
